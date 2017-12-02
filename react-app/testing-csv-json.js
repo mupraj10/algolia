3fc9f39f0083c80ab716fbf827370bd5f379e5e2
Maggy Prajapati <mupraj10@gmail.com>
2017-12-02 18:36:44 -0500

    setting added and add/update test dataset working

--- react-app/testing-csv-json.js
+++ react-app/testing-csv-json.js
@@ -1,28 +1,27 @@
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
 //csv converter module
-const csvjson = require("csvjson");
+const csvjson = require('csvjson');
 
-const algoliasearch = require("algoliasearch");
+//algolia api to upload data to be indexed
+const algoliasearch = require('algoliasearch');
 
 //algolia secrets
-const applicationId = "BYAMNN0H16";
-const apiKey = "df7062298c712bb60acc38d2ecfdee8c";
+const applicationId = 'BYAMNN0H16';
+const apiKey = 'df7062298c712bb60acc38d2ecfdee8c';
 
 const client = algoliasearch(applicationId, apiKey);
-const index = client.initIndex("test");
+const index = client.initIndex('test');
 
 //import helper function
-const updatePaymentOptions = require("./helperfunc");
-
-// headers = ['objectID','food_type','stars_count','reviews_count','neighborhood','phone_number','price_range','dining_style']
+const updatePaymentOptions = require('./helperfunc');
 
 //converting CSV data into string for module
 const csvData = fs.readFileSync(
-  path.join(__dirname, "restaurants_info_test.csv"),
-  { encoding: "utf8" }
+  path.join(__dirname, 'restaurants_info_test.csv'),
+  { encoding: 'utf8' }
 );
 
 //options for removing the :|,
@@ -33,42 +32,82 @@ const options = {
 //converting string into array of objects
 const formattedData = csvjson.toObject(csvData, options);
 
-// see the formated data
+// see the formated csv data
 // console.log(formattedData);
-
-// add CSV data to algolia index
-index.addObjects(formattedData, (err, content) =>
-  console.log(
-    `Successfully added ${chalk.red("CSV")} into algolia ${chalk.cyan(
-      index.indexName
-    )} index:`,
-    content
-  )
-);
 
 // accessing json data in this file
 const jsonData = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "restaurants_list_test.json"), {
-    encoding: "utf8"
+  fs.readFileSync(path.join(__dirname, 'restaurants_list_test.json'), {
+    encoding: 'utf8'
   })
 );
 
-//see the data
+//see the json data
 // console.log('json data', jsonData)
 
 // manipulate the JSON data
+
 // to remove the payment methods and change to discover using helper functions
 const updatedJson = updatePaymentOptions(jsonData, { log: true });
 // change the stars?
 
-// add json data to algolia index
-index.addObjects(updatedJson, (err, content) =>
-  console.log(
-    `Successfully added ${chalk.red("JSON")}  data into algolia ${chalk.cyan(
-      index.indexName
-    )} index:`,
-    content.objectIDs
-  )
-);
+const settings = {
+  searchableAttributes: [
+    'name',
+    'food_type',
+    'city',
+    'area',
+    'country',
+    'postal_code',
+    'state',
+    'neighborhood'
+  ],
+  attributesForFaceting: ['food_type', 'stars_count', 'acceptable_payments']
+};
+
+//setting the search settings
+index.setSettings(settings, err => {
+  !err
+    ? console.log(chalk.red('Sucessfully set settings!'))
+    : console.log(chalk.red(err));
+});
+
+// add  CSV data to algolia index
+index.addObjects(formattedData, (err, content) => {
+  !err
+    ? console.log(
+        `Successfully added ${chalk.red('CSV')} data into algolia ${chalk.cyan(
+          index.indexName
+        )} index:`,
+        content
+      )
+    : console.log(chalk.red(err));
+});
+
+// update to algolia index with json data
+index.partialUpdateObjects(updatedJson, (err, content) => {
+  !err
+    ? console.log(
+        `Successfully updated with ${chalk.red(
+          'JSON'
+        )} data into algolia ${chalk.cyan(index.indexName)} index:`,
+        content.objectIDs
+      )
+    : console.log(chalk.red(err));
+});
 
 
+//notes
+
+//find out why csv data goes faster than json data?
+
+// headers = ['objectID','food_type','stars_count','reviews_count','neighborhood','phone_number','price_range','dining_style']
+
+//can also implement batching if number of items is higher than 1000 ?
+
+// search settings
+//should be able to search by name, food_type and location(city, area)
+//filter/facetting by food_type, stars_count, acceptable_payments
+
+// from algolia website, goog to set settings before pushing the data
+// https://www.algolia.com/doc/api-reference/api-methods/set-settings/
