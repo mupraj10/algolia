397ad0161f2e2941688d2d1775a820fcbdd923ba
Maggy Prajapati <mupraj10@gmail.com>
2017-12-03 22:02:18 -0500

    working search

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      hits: []
+      hits: [], 
+      facets: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.applyLocation = this.applyLocation.bind(this);
@@ -20,13 +21,19 @@ class App extends Component {
     }`;
     // console.log(location);
     helper.setQueryParameter("aroundLatLng", location).search();
-    helper.on("result", content => this.setState({ hits: content.hits }));
+    helper.on("result", content => {
+      console.log('content', content.facets);
+      this.setState({ hits: content.hits, facets: content.facets })
+    });
   }
+
   defaultLocation() {
     console.log("Defaulting location to NYC");
     const nycCoords = "40.7128, 74.0060";
     helper.setQueryParameter("aroundLatLng", nycCoords).search();
-    helper.on("result", content => this.setState({ hits: content.hits }));
+    helper.on("result", content => {
+      this.setState({ hits: content.hits, facets: content.facets  })
+    });
   }
 
   componentDidMount() {
@@ -39,26 +46,28 @@ class App extends Component {
     }
   }
 
+
   handleChange(evt) {
     evt.preventDefault();
     helper.setQuery(evt.target.value).search();
-
     helper.on("result", hits => this.setState({ hits: hits.hits }));
   }
 
   render() {
     const hits = this.state.hits;
+    const facets = this.state.facets;
+    const facetList = Object.keys(`facets.${facets.name}.data`)
+
     console.log('type as you search');
     return (
       <div className="App">
         <div>
           <input
           className="search-box"
-            onChange={this.handleChange}
-            placeholder={"Search for Restaurants by Name, Cuisine, Location"}
+          onChange={this.handleChange}
+          placeholder={"Search for Restaurants by Name, Cuisine, Location"}
           />
         </div>
-
         {hits && hits.map(hit => <h2 key={Math.random()}>{hit.name}</h2>)}
       </div>
     );
@@ -70,3 +79,14 @@ export default App;
 //notes
 //figure out on submit
 
+
+//to get the list of facets
+
+//content.facets.name => name 
+//content.facets.data => {key:id}
+// {facets && facets.map(facet => (<div> <h3>{facet.name}</h3> {facetList.map(type => <input type='checkbox'></input> )} </div>)
+// ) }
+
+// {facets && facets.map(facet => (<div> <h3>{facet.name}</h3> {facetList.map(type =>  <div>  <input type='checkbox'/> <label> {facetList[type]} </label> </div> )} 
+// </div>)
+//  )}
--- react-app/src/algolia.js
+++ react-app/src/algolia.js
@@ -7,7 +7,8 @@ const applicationID = 'BYAMNN0H16'
 const apiKey = '75a10d692eba6953c2c798c1c63df046'
 
 const indexName = 'restaurants'
+const facets = {facets:['food_type', 'stars_count', 'acceptable_payments']}
 
 export const client = algoliasearch(applicationID, apiKey);
-export const helper = algoliasearchHelper(client, indexName);
+export const helper = algoliasearchHelper(client, indexName, facets);
 
