a0b11a5a29e02fc2ae518f51db9dd56e8bcfe718
Maggy Prajapati <mupraj10@gmail.com>
2017-12-06 11:59:37 -0500

    import function and cc adjustments done

---
+++ project-files/resources/dataset/helperfunc.js
@@ -0,0 +1,71 @@
+const chalk = require("chalk");
+
+//helper function that if it has a Diners Club card or a Carte Blanche card it switches it Discover if it doesn't already there
+
+function updatePaymentOptions(jsonObj, log) {
+  const updatedJson = [];
+  //iterate through the JSON array for each place
+  jsonObj.forEach(place => {
+    const acceptablePayments = ["AMEX", "MasterCard", "Discover", "Visa"];
+    const replacingPayments = ["Diners Club", "Carte Blanche"];
+
+    const currentPaymentOptions = place.payment_options;
+
+    //create a copy of the payment options
+    let updatedPayments = currentPaymentOptions.slice();
+
+    // iterate through to find the card types
+    replacingPayments.forEach(cardType => {
+      //if Diner's club or Carte Blanche is present manipulate
+      if (updatedPayments.indexOf(cardType) !== -1) {
+        //find in payments array and remove
+        const positionInArray = updatedPayments.indexOf(cardType);
+        const removed = updatedPayments.splice(positionInArray, 1);
+
+        //if discover is not there add it in
+        if (!updatedPayments.includes("Discover")) {
+          //   console.log(chalk.green('adding in Discover'));
+          updatedPayments.push("Discover");
+        }
+
+        //   console.log(
+        //     "foundCard",
+        //     chalk.blue(cardType),
+        //     "at position",
+        //     chalk.red(positionInArray),
+        //     "removed",
+        //     chalk.magenta(removed),
+        //     "new payment options",
+        //     chalk.red(updatedPayments)
+        //   );
+      }
+      //if does not have Diners club or Carte Blanche it is just a copy same
+      //adding in filtering to make sure only those options
+
+      updatedPayments = updatedPayments.filter(card => acceptablePayments.includes(card));
+      return updatedPayments;
+    });
+
+    //maybe it customer wanted to see the logs of what changed
+    // if(log.true){
+      //   console.log('updated', 
+      //   chalk.magenta(place.name),
+      //   "original",
+      //   chalk.red(currentPaymentOptions),
+      //   "updated and returned",
+      //   chalk.cyan(updatedPayments)
+      // );
+    // }
+
+    //now add in JSON object a new option for acceptable_payments
+    let acceptablePaymentAdded = Object.assign({}, place, {acceptable_payments: updatedPayments});
+    updatedJson.push(acceptablePaymentAdded)
+  });
+//   console.log('updatedJson', updatedJson)
+  return updatedJson;
+}
+
+// updatePaymentOptions(testJson);
+ 
+//figure out how to export function
+module.exports = updatePaymentOptions;
